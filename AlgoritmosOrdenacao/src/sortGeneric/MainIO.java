dc8896df835c3c7938a0c3e37d7d42354394dfdc
Ana Paula Barros <barros.anapaulas@gmail.com>
2016-02-22 18:38:52 -0300

    Testes de casos

--- AlgoritmosOrdenacao/src/sortGeneric/MainIO.java
+++ AlgoritmosOrdenacao/src/sortGeneric/MainIO.java
@@ -5,9 +5,9 @@ public class MainIO {
 	public static void main(String[] args) {
 		Sort<Integer> teste = new Sort<>();
 		Integer[] newObj = {3,1,4,2,5};
-		String[]  newStr = {"b", "c", "a", "a"};
-		teste.selectionSort(newObj);
-		for (Integer s : newObj) {
+		Integer[]  newStr = {10, 9, 8, 7, 6, 5, 4, 3, -1, -1};
+		teste.insertionSort(newStr);
+		for (Integer s : newStr) {
 			System.out.print(s + " ");
 		}
 	}
