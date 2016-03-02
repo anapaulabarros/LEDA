9a518c3ea9da9085038ebb2237cf3975b0ed5c80
Ana Paula Barros <barros.anapaulas@gmail.com>
2016-03-02 00:51:23 -0300

    Radix Sort e Couting Sort por digito

--- AlgoritmosOrdenacao/src/ordenacaoLinear/CountingSort.java
+++ AlgoritmosOrdenacao/src/ordenacaoLinear/CountingSort.java
@@ -1,9 +1,17 @@
 package ordenacaoLinear;
 
+import java.util.Arrays;
+
 public class CountingSort {
 
-	public CountingSort(){}
-	
+public CountingSort(){}
+
+	/**
+	 * Metodo couting Sort tradicional
+	 * @param int[] Array
+	 * @param int max - maior valor encontrado dentro do array
+	 * @return int[] array auxiliar ordenado
+	 * */
 	public int[] sort(int[] A, int max){
 		int[] vetorContador = new int[max]; //inicia o vetor auxiliar
 		int[] vetorAux = new int[A.length]; //array final com valores ordenados
@@ -12,7 +20,6 @@ public class CountingSort {
 		for(int i = 0; i < A.length; i++) {
 			vetorContador[A[i]-1]++;
 		}
-		
 		/*Soma acumulativa*/
 		for(int i = 1; i < vetorContador.length; i++) {
 			vetorContador[i] = vetorContador[i-1] + vetorContador[i];
@@ -27,4 +34,36 @@ public class CountingSort {
 		return vetorAux;
 		
 	}
+	
+	/**
+	 * Metodo couting Sort por dígito
+	 * @param int[] Array
+	 * @param int max - maior valor encontrado dentro do array
+	 * @return void 
+	 * */
+	public void sortPerDigit(int arr[], int exp)
+    {
+		int output[] = new int[arr.length]; // array aux
+        int count[] = new int[10]; // array com os digitos
+ 
+        //armazenamento de contador de ocorrencia em count[] 
+        for (int i = 0; i < arr.length; i++)
+            count[ (arr[i]/exp)%10 ]++;
+ 
+        // contagem dos digitos
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+ 
+        // Build the output array
+        for (int i = arr.length - 1; i >= 0; i--)
+        {
+            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+            count[ (arr[i]/exp)%10 ]--;
+        }
+ 
+        // Copia os valores do array output para arr[], assim o array original
+        // conterá os seus valores ordenados
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = output[i];
+    }
 }
--- AlgoritmosOrdenacao/src/ordenacaoLinear/RadixSort.java
+++ AlgoritmosOrdenacao/src/ordenacaoLinear/RadixSort.java
@@ -1,10 +1,27 @@
 package ordenacaoLinear;
 
-public class RadixSort {
+import java.util.Arrays;
 
-	private CountingSort counting;
+public class RadixSort {
 	
-	public RadixSort() {
-		counting = new CountingSort();
+	private CountingSort couting;
+	
+	public RadixSort(){
+		couting = new CountingSort();
 	}
+	
+	public int getMaxValue(int[] A){
+		return Arrays.stream(A).max().getAsInt();
+	}
+	
+	public void sorting(int[] arr, int sizeArray){
+
+		//encontra o máximo valor no vetor
+        int max = getMaxValue(arr);
+ 
+        //Ordena por digito usando o CountingSort
+        for (int digit = 1; max/digit > 0; digit *= 10)
+            couting.sortPerDigit(arr, digit);
+	}
+	
 }
